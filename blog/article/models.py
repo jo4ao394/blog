e2a498e9e850b6c4b08e16febcc4cf6d78782aef
jo4ao394 <eav213041@gmail.com>
2017-12-20 13:23:04 +0800

    12/20

--- blog/article/models.py
+++ blog/article/models.py
@@ -16,7 +16,10 @@ class Article(models.Model):
 class Comment(models.Model):
     article = models.ForeignKey(Article)
     content = models.CharField(max_length=128)
-    
+    pubDateTime = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.article.title + '-' + str(self.id)
+    class Meta:
+        ordering = ['-pubDateTime']
--- blog/article/views.py
+++ blog/article/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from article.models import Article, Comment
 
-# Create your views here.
+
+def article(request):
+    '''
+    Render the article page
+    '''
+    articles = Article.objects.all()
+    context = {'articles':articles}
+    return render(request, 'article/article.html', context)
